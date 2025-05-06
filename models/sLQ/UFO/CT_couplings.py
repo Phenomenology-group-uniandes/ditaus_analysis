8b2c6e7723422b7953b905369120ce933d4be259
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2025-05-06 01:04:30 +0000

    ADD Feynrule Models

---
+++ models/sLQ/UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 8.0 for Mac OS X x86 (64-bit) (October 5, 2011)
+# Date: Fri 9 Feb 2018 16:10:23
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
